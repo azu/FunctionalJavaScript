593fd2b48441b8ec650f04be7c5e3ba380ace0ae
azu <azuciao@gmail.com>
2013-09-16 17:29:47 +0900

    dipatch pattern

---
+++ Chapter5/Ch5.Function-Building-Functions.js
@@ -0,0 +1,102 @@
+/**
+ * Created by azu on 2013/09/16.
+ */
+
+"use strict";
+
+var assert = require('chai').assert;
+var _ = require("underscore");
+var existy = require("../lib/existy.js").existy;
+var invoker = require("../lib/invoker.js").invoker;
+var construct = require("../lib/construct.js").construct;
+var always = require("../lib/always.js").always;
+
+/*
+    invoker と合わせて使う
+
+    複数のinvokeする関数を渡して、順番の実行していきちゃんと値が返ってきた時点でそれを採用する
+ */
+function dispatch(/* funs */) {
+    var funcs = _.toArray(arguments);
+    var size = funcs.length;
+    return function (target /*args*/) {
+        var ret = undefined;
+        var args = _.rest(arguments);
+        for (var funcIndex = 0; funcIndex < size; funcIndex++) {
+            var func = funcs[funcIndex];
+            ret = func.apply(func, construct(target, args));
+            if (existy(ret)) {
+                return ret;
+            }
+        }
+        return ret;
+    };
+}
+
+describe("dispatch", function () {
+    it("invoker", function () {
+        var str = dispatch(invoker('toString', Array.prototype.toString), invoker('toString', String.prototype.toString));
+        assert.equal(str("a"), "a");
+        assert.equal(str(_.range(10)), "0,1,2,3,4,5,6,7,8,9");
+    });
+});
+
+/*
+   dispatchはinvokerに依存しているわけではないが、 ``construct`` には依存してる。
+
+   invoker を使わない関数をdispatchには渡せて、次のような stringReverse を考えてみる。
+ */
+function stringReverse(s) {
+    if (!_.isString(s)) {
+        return undefined;
+    }
+    return s.split("").reverse().join("");
+}
+describe("stringReverse", function () {
+    it("return reverse string", function () {
+        var reverseString = stringReverse("abc");
+        assert.equal(reverseString, "cba");
+    });
+    context("when pass number", function () {
+        it("should return undefined", function () {
+            var results = stringReverse(1);
+            assert.isUndefined(results);
+        });
+    });
+});
+
+/*
+    stringReverseも組み合わせてみると
+ */
+it("arrayとstringのreverse", function () {
+    var rev = dispatch(invoker('reverse', Array.prototype.reverse), stringReverse);
+    /*
+        Arrayのreverseをして、それができないならstringのreverseをするという ``rev``
+     */
+    assert.deepEqual(rev([1, 2, 3]), [3, 2, 1]);
+    assert.equal(rev("str"), "rts");
+
+    /*
+    このrev自体も更に発展させて compose する事ができるので、
+
+    alwaysのような常に値が変えるような関数と組み合わせれば、デフォルト値みたいな感じに使える
+     */
+    var sillyReverse = dispatch(rev, always(42));
+    assert.deepEqual(sillyReverse([1, 2, 3]), [3, 2, 1]);
+    assert.equal(sillyReverse("str"), "rts");
+    // 数値のreverseは実装してないので, alwaysが返す
+    assert.equal(sillyReverse(100000), 42);
+});
+
+function performCommandHardcoded(command) {
+    var result;
+    switch (command.type) {
+        case 'notify':
+            break;
+        case 'join':
+            break;
+        default :
+            break;
+    }
+    return result;
+}
